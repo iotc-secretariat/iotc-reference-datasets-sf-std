a0ead833fbb8bed8901163115dd4c7fa4596d01f
flowerside <flowerside@gmail.com>
2024-02-16 11:43:35 +0400

    Staging latest updates including RDAs

--- R/exported_functions.R
+++ R/exported_functions.R
@@ -1,19 +1,19 @@
 #' Returns a dataset containing standardized size-frequency data for all categories of species
 #' @export
-all = function() {
-  return(
+SF.STD.all = function() {
+  ALL =
     rbind(
-      STD.BET,
-      STD.SKJ,
-      STD.YFT,
-      STD.TEMP,
-      STD.BILL,
-      STD.NERI,
-      STD.SEER,
-      #STD.TNEI,
-      STD.SHRK,
-      #STD.OTHR,
-      #STD.ETPS
+      SF.STD.TEMP,
+      SF.STD.TROP,
+      SF.STD.BILL,
+      SF.STD.NERI,
+      SF.STD.SEER,
+      SF.STD.SHRK
     )
-  )
+
+  if(exists("SF.STD.TNEI") & !is.null(SF.STD.TNEI)) ALL = rbind(ALL, SF.STD.TNEI)
+  if(exists("SF.STD.ETPS") & !is.null(SF.STD.ETPS)) ALL = rbind(ALL, SF.STD.ETPS)
+  if(exists("SF.STD.OTHR") & !is.null(SF.STD.OTHR)) ALL = rbind(ALL, SF.STD.OTHR)
+
+  return(ALL)
 }
--- data-raw/load_datasets.R
+++ data-raw/load_datasets.R
@@ -1,43 +1,95 @@
 library(iotc.base.common.data)
+library(iotc.base.common.std)
 
-SF_RAW_TEMP = SF.raw(species_category_codes = "TEMPERATE")
-usethis::use_data(SF_RAW_TEMP, overwrite = TRUE)
+ALL_SPECIES =
+  query(
+    connection = DB_IOTDB(),
+    query = "
+    SELECT DISTINCT  --DISTINCT added by manu 24/04/2023
+      E.[Group] AS SPECIES_GROUP_CODE,
+      E.Species AS SPECIES_CODE,
+      S.NAME_EN AS SPECIES_NAME_EN,
+      S.NAME_LT AS SPECIES_SCIENTIFIC_NAME,
+      E.StdSizeCode AS LENGTH_CODE,
+      CASE
+        WHEN E.StdSizeCode = 'FL' THEN 'Fork length'
+        WHEN E.StdSizeCode = 'EFL' THEN 'Eye-fork length'
+  	  ELSE
+        M.EngDescr
+    	END AS LENGTH_NAME_EN,
+      E.MaxStdSize AS MAX_SIZE,
+      E.StdSizeClassInt AS STANDARD_BIN_SIZE,
+      E.MaxSizeClassInt AS MAX_BIN_SIZE,
+      E.FirstSizeClass AS FIRST_SIZE_CLASS
+    FROM
+      estSpecies E
+    LEFT JOIN
+      [IOTDB].[meta].SPECIES S
+    ON
+      E.Species = S.CODE
+    LEFT JOIN
+    	[IOTDB].[dbo].cdeMeasTypes M
+    ON
+    	E.StdSizeCode = M.ACode
+    WHERE
+      --S.IOTDB_CODE IN ('BET', 'SKJ', 'YFT') AND
+      S.IOTDB_CODE NOT IN ('SKJS', 'SKJM', 'SKJL',
+                           'YFTS', 'YFTM', 'YFTL')
+    ")
 
-SF_RAW_TROP = SF.raw(species_category_codes = "TROPICAL")
-usethis::use_data(SF_RAW_TROP, overwrite = TRUE)
+process_species = function(dataset) {
+  all_species = unique(dataset$SPECIES_CODE)
 
-SF_RAW_BILL = SF.raw(species_category_codes = "BILLFISH")
-usethis::use_data(SF_RAW_BILL, overwrite = TRUE)
+  standardized_dataset = NULL
 
-SF_RAW_NERI = SF.raw(species_category_codes = "NERITIC")
-usethis::use_data(SF_RAW_NERI, overwrite = TRUE)
+  for(species in all_species) {
+    species_data = dataset[SPECIES_CODE == species]
 
-SF_RAW_SEER = SF.raw(species_category_codes = "SEERFISH")
-usethis::use_data(SF_RAW_SEER, overwrite = TRUE)
+    if(species %in% ALL_SPECIES$SPECIES_CODE) {
+      standardized_species_data =
+        standardize_and_pivot_size_frequencies(
+          species_data,
+          bin_size        = ALL_SPECIES[SPECIES_CODE == species]$STANDARD_BIN_SIZE,
+          max_bin_size    = ALL_SPECIES[SPECIES_CODE == species]$MAX_BIN_SIZE,
+          first_class_low = ALL_SPECIES[SPECIES_CODE == species]$FIRST_SIZE_CLASS
+        )
 
-SF_RAW_TNEI = SF.raw(species_category_codes = "TUNAS_NEI")
-usethis::use_data(SF_RAW_TNEI, overwrite = TRUE)
+      if(is.null(standardized_dataset))
+        standardized_dataset = standardized_species_data
+      else
+        standardized_dataset = rbind(standardized_dataset, standardized_species_data)
+    }
+  }
 
-SF_RAW_SHRK = SF.raw(species_category_codes = c("SHARKS", "RAYS"))
-usethis::use_data(SF_RAW_SHRK, overwrite = TRUE)
+  return(standardized_dataset)
+}
 
-SF_RAW_OTHR = SF.raw(species_category_codes = c("OTHERS"))
-usethis::use_data(SF_RAW_OTHR, overwrite = TRUE)
+SF.STD.TEMP = process_species(SF.raw(species_category_codes = "TEMPERATE"))
+usethis::use_data(SF.STD.TEMP, overwrite = TRUE, compress = "gzip")
 
-SF_RAW_ETPS = SF.raw(species_category_codes = c("CETACEANS", "SEABIRDS", "TURTLES"))
-usethis::use_data(SF_RAW_ETPS, overwrite = TRUE)
+SF.STD.TROP = process_species(SF.raw(species_category_codes = "TROPICAL"))
+usethis::use_data(SF.STD.TROP, overwrite = TRUE, compress = "gzip")
 
-SF_RAW_ALL =
-  rbind(
-    SF_RAW_TEMP,
-    SF_RAW_TROP,
-    SF_RAW_BILL,
-    SF_RAW_NERI,
-    SF_RAW_SEER,
-    SF_RAW_TNEI,
-    SF_RAW_SHRK,
-    SF_RAW_OTHR,
-    SF_RAW_ETPS
-  )
+SF.STD.BILL = process_species(SF.raw(species_category_codes = "BILLFISH"))
+usethis::use_data(SF.STD.BILL, overwrite = TRUE, compress = "gzip")
 
-unique(SF_RAW_BILL$MEASU[
+SF.STD.NERI = process_species(SF.raw(species_category_codes = "NERITIC"))
+usethis::use_data(SF.STD.NERI, overwrite = TRUE, compress = "gzip")
+
+SF.STD.SEER = process_species(SF.raw(species_category_codes = "SEERFISH"))
+usethis::use_data(SF.STD.SEER, overwrite = TRUE, compress = "gzip")
+
+SF.STD.TNEI = process_species(SF.raw(species_category_codes = "TUNAS_NEI"))
+if(!is.null(SF.STD.TNEI)) usethis::use_data(SF.STD.TNEI, overwrite = TRUE, compress = "gzip")
+
+SF.STD.SHRK = process_species(SF.raw(species_category_codes = c("SHARKS", "RAYS")))
+usethis::use_data(SF.STD.SHRK, overwrite = TRUE, compress = "gzip")
+
+SF.STD.ETPS = process_species(SF.raw(species_category_codes = c("CETACEANS", "SEABIRDS", "TURTLES")))
+if(!is.null(SF.STD.ETPS)) usethis::use_data(SF.STD.ETPS, overwrite = TRUE, compress = "gzip")
+
+SF.STD.OTHR = process_species(SF.raw(species_category_codes = c("OTHERS")))
+if(!is.null(SF.STD.OTHR)) usethis::use_data(SF.STD.OTHR, overwrite = TRUE, compress = "gzip")
+
+LAST_UPDATE = Sys.Date()
+usethis::use_data(LAST_UPDATE, overwrite = TRUE, compress = "gzip")
